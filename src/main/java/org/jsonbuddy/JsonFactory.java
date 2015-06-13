8e46b237488478e4d8e96de2fb4591b32142cf31
Anders Karlsen <karlsenanders@gmail.com>
2015-06-13 07:33:01 +0200

    Factories gone

--- src/main/java/org/jsonbuddy/JsonFactory.java
+++
@@ -1,7 +0,0 @@
-package org.jsonbuddy;
-
-
-public abstract class JsonFactory {
-
-
-}
--- src/test/java/org/jsonbuddy/JsonBuildTest.java
+++ src/test/java/org/jsonbuddy/JsonBuildTest.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class JsonFactoryTest {
+public class JsonBuildTest {
 
     @Test
     public void shouldCreateValue() throws Exception {
