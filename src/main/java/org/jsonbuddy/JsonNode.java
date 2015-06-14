e00b38f7981d1ad1d6d98540bf3008642caa70da
Anders Karlsen <karlsenanders@gmail.com>
2015-06-14 10:02:34 +0200

    Added a little javadoc

--- src/main/java/org/jsonbuddy/JsonNode.java
+++ src/main/java/org/jsonbuddy/JsonNode.java
@@ -4,12 +4,21 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public abstract class JsonNode {
+
+    /**
+     * The values as json string
+     * @return
+     */
     public String toJson() {
         StringWriter res = new StringWriter();
         toJson(new PrintWriter(res));
         return res.toString();
     }
 
+    /**
+     * Writes this objects as json to the given writer
+     * @param printWriter
+     */
     public abstract void toJson(PrintWriter printWriter);
 
     public <T> T as(Class<T> clazz) {
@@ -20,12 +29,22 @@ public abstract class JsonNode {
     }
 
 
+    /**
+     * Read the value of an attribute from a JsonObject
+     * @param key The given key
+     * @return The value of the attribute
+     * @throws JsonValueNotPresentException If this object is not a Json object, or if a value with the given key does not exist.
+     */
     public String requiredString(String key) throws JsonValueNotPresentException {
         throw new JsonValueNotPresentException(String.format("Required key '%s' does not exsist",key));
     }
 
-    public String textValue() {
+    public String textValue() throws JsonValueNotPresentException {
         throw new JsonValueNotPresentException(String.format("Not supported for class %s",getClass().getSimpleName()));
+    }
 
+    @Override
+    public String toString() {
+        return toJson();
     }
 }
