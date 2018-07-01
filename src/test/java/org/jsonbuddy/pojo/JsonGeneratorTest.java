d5fb852cf942a82b2a64904742b55b819c7709a6
Anders Karlsen <karlsenanders@gmail.com>
2018-07-01 18:34:38 +0200

    JsonGenderator with two methods (one for declaring class as template one for implementing class as template). Removed deprecated enum PojoMappingOption

--- src/test/java/org/jsonbuddy/pojo/JsonGeneratorTest.java
+++ src/test/java/org/jsonbuddy/pojo/JsonGeneratorTest.java
@@ -18,7 +18,7 @@ public class JsonGeneratorTest {
     @Test
     public void shouldHandleSimpleClass() throws Exception {
         SimpleWithName simpleWithName = new SimpleWithName("Darth Vader");
-        JsonNode generated = JsonGenerator.generate(simpleWithName);
+        JsonNode generated = JsonGenerator.generateUsingImplementationAsTemplate(simpleWithName);
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
         assertThat(jsonObject.stringValue("name").get()).isEqualTo("Darth Vader");
@@ -26,15 +26,15 @@ public class JsonGeneratorTest {
 
     @Test
     public void shouldHandleSimpleValues() throws Exception {
-        assertThat(JsonGenerator.generate(null)).isEqualTo(new JsonNull());
-        assertThat(JsonGenerator.generate("Darth")).isEqualTo(JsonFactory.jsonString("Darth"));
-        assertThat(JsonGenerator.generate(42)).isEqualTo(JsonFactory.jsonNumber(42));
+        assertThat(JsonGenerator.generateUsingImplementationAsTemplate(null)).isEqualTo(new JsonNull());
+        assertThat(JsonGenerator.generateUsingImplementationAsTemplate("Darth")).isEqualTo(JsonFactory.jsonString("Darth"));
+        assertThat(JsonGenerator.generateUsingImplementationAsTemplate(42)).isEqualTo(JsonFactory.jsonNumber(42));
 
     }
 
     @Test
     public void shoulHandleFloats() throws Exception {
-        JsonNode jsonNode = JsonGenerator.generate(3.14f);
+        JsonNode jsonNode = JsonGenerator.generateUsingImplementationAsTemplate(3.14f);
         JsonNumber jsonDouble = (JsonNumber) jsonNode;
         assertThat(new Double(jsonDouble.doubleValue()).floatValue()).isEqualTo(3.14f);
     }
@@ -43,7 +43,7 @@ public class JsonGeneratorTest {
     public void shouldHandleList() throws Exception {
         List<String> stringlist = Arrays.asList("one", "two", "three");
 
-        JsonNode generate = JsonGenerator.generate(stringlist);
+        JsonNode generate = JsonGenerator.generateUsingImplementationAsTemplate(stringlist);
         assertThat(generate).isInstanceOf(JsonArray.class);
         JsonArray array = (JsonArray) generate;
         assertThat(array.strings()).isEqualTo(stringlist);
@@ -53,7 +53,7 @@ public class JsonGeneratorTest {
     public void shouldHandleArray() throws Exception {
         String[] stringarray = { "one", "two", "three" };
 
-        JsonNode generate = JsonGenerator.generate(stringarray);
+        JsonNode generate = JsonGenerator.generateUsingImplementationAsTemplate(stringarray);
         assertThat(generate).isInstanceOf(JsonArray.class);
         JsonArray array = (JsonArray) generate;
         assertThat(array.strings()).containsExactly(stringarray);
@@ -62,7 +62,7 @@ public class JsonGeneratorTest {
     @Test
     public void shouldHandleListWithClasses() throws Exception {
         List<SimpleWithName> simpleWithNames = Arrays.asList(new SimpleWithName("Darth"), new SimpleWithName("Anakin"));
-        JsonArray array = (JsonArray) JsonGenerator.generate(simpleWithNames);
+        JsonArray array = (JsonArray) JsonGenerator.generateUsingImplementationAsTemplate(simpleWithNames);
 
         List<JsonObject> objects = array.objects(o -> o);
 
@@ -76,7 +76,7 @@ public class JsonGeneratorTest {
         combinedClassWithSetter.setPerson(new SimpleWithName("Darth Vader"));
         combinedClassWithSetter.setOccupation("Dark Lord");
 
-        JsonObject jsonObject = (JsonObject) JsonGenerator.generate(combinedClassWithSetter);
+        JsonObject jsonObject = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(combinedClassWithSetter);
 
         assertThat(jsonObject.stringValue("occupation").get()).isEqualTo("Dark Lord");
         Optional<JsonObject> person = jsonObject.objectValue("person");
@@ -91,7 +91,7 @@ public class JsonGeneratorTest {
     @Test
     public void shouldHandleOverriddenValues() throws Exception {
         JsonGeneratorOverrides overrides = new JsonGeneratorOverrides();
-        JsonObject generate = (JsonObject) JsonGenerator.generate(overrides);
+        JsonObject generate = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(overrides);
 
         assertThat(generate.requiredLong("myOverriddenValue")).isEqualTo(42);
 
@@ -102,7 +102,7 @@ public class JsonGeneratorTest {
         ClassWithJsonElements classWithJsonElements = new ClassWithJsonElements("Darth Vader",
                 JsonFactory.jsonObject().put("title", "Dark Lord"),
                 JsonFactory.jsonArray().add("Luke").add("Leia"));
-        JsonObject generate = (JsonObject) JsonGenerator.generate(classWithJsonElements);
+        JsonObject generate = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(classWithJsonElements);
 
         assertThat(generate.requiredString("name")).isEqualTo("Darth Vader");
         assertThat(generate.requiredObject("myObject").requiredString("title")).isEqualTo("Dark Lord");
@@ -114,14 +114,14 @@ public class JsonGeneratorTest {
         Map<String, String> map = new HashMap<>();
         map.put("name", "Darth Vader");
         ClassWithMap classWithMap = new ClassWithMap(map);
-        JsonObject generate = (JsonObject) JsonGenerator.generate(classWithMap);
+        JsonObject generate = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(classWithMap);
         assertThat(generate.requiredObject("properties").requiredString("name")).isEqualTo("Darth Vader");
     }
 
     @Test
     public void shouldHandleDifferentSimpleTypes() throws Exception {
         ClassWithDifferentTypes classWithDifferentTypes = new ClassWithDifferentTypes("my text", 42, true, false);
-        JsonObject generated = (JsonObject) JsonGenerator.generate(classWithDifferentTypes);
+        JsonObject generated = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(classWithDifferentTypes);
         assertThat(generated.requiredString("text")).isEqualTo("my text");
         assertThat(generated.requiredLong("number")).isEqualTo(42);
         assertThat(generated.requiredBoolean("bool")).isTrue();
@@ -133,28 +133,28 @@ public class JsonGeneratorTest {
         ClassWithTime classWithTime = new ClassWithTime();
         classWithTime.setTime(dateTime.toInstant());
 
-        JsonObject jsonNode = (JsonObject) JsonGenerator.generate(classWithTime);
+        JsonObject jsonNode = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(classWithTime);
         assertThat(jsonNode.requiredString("time")).isEqualTo("2015-08-13T21:14:18.000000321Z");
     }
 
     @Test
     public void shouldHandleClassWithEnum() throws Exception {
         ClassWithEnum classWithEnum = new ClassWithEnum();
-        JsonObject jso = (JsonObject) JsonGenerator.generate(classWithEnum);
+        JsonObject jso = (JsonObject) JsonGenerator.generateUsingImplementationAsTemplate(classWithEnum);
         assertThat(jso.requiredString("enumNumber")).isEqualTo("ONE");
     }
 
     @Test
     public void shouldHandleNumbers() throws Exception {
-        assertThat(JsonGenerator.generate(12L)).isEqualTo(new JsonNumber(12L));
-        assertThat(JsonGenerator.generate(12)).isEqualTo(new JsonNumber(12));
+        assertThat(JsonGenerator.generateUsingImplementationAsTemplate(12L)).isEqualTo(new JsonNumber(12L));
+        assertThat(JsonGenerator.generateUsingImplementationAsTemplate(12)).isEqualTo(new JsonNumber(12));
     }
 
     @Test
     public void shouldHandleMapWithKeyOtherThanString() throws Exception {
         Map<Long,String> myLongMap = new HashMap<>();
         myLongMap.put(42L, "Meaning of life");
-        JsonNode generated = JsonGenerator.generate(myLongMap);
+        JsonNode generated = JsonGenerator.generateUsingImplementationAsTemplate(myLongMap);
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
         assertThat(jsonObject.requiredString("42")).isEqualTo("Meaning of life");
@@ -165,7 +165,7 @@ public class JsonGeneratorTest {
         ClassWithBigNumbers bn = new ClassWithBigNumbers();
         bn.setOneBigInt(BigInteger.valueOf(42L));
 
-        JsonNode generated = JsonGenerator.generate(bn);
+        JsonNode generated = JsonGenerator.generateUsingImplementationAsTemplate(bn);
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
         assertThat(jsonObject.requiredLong("oneBigInt")).isEqualTo(42L);
@@ -177,7 +177,7 @@ public class JsonGeneratorTest {
         ClassWithBigNumbers bn = new ClassWithBigNumbers();
         bn.setOneBigDec(BigDecimal.valueOf(3.14d));
 
-        JsonNode generated = JsonGenerator.generate(bn);
+        JsonNode generated = JsonGenerator.generateUsingImplementationAsTemplate(bn);
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
         assertThat(jsonObject.requiredDouble("oneBigDec")).isEqualTo(3.14d);
@@ -188,7 +188,7 @@ public class JsonGeneratorTest {
     public void shouldMaskMethodsNotInInterfaceWhenUsingGetter() throws Exception {
         InterfaceWithMethod myInterface = new ClassImplementingInterface("myPublic", "mySecret");
         ClassWithGetterInterface classWithGetterInterface = new ClassWithGetterInterface(myInterface);
-        JsonNode generated = JsonGenerator.generate(classWithGetterInterface, PojoMapOption.USE_INTERFACE_FIELDS);
+        JsonNode generated = JsonGenerator.generate(classWithGetterInterface);
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
         JsonObject childObj = jsonObject.requiredObject("myInterface");
@@ -200,7 +200,7 @@ public class JsonGeneratorTest {
     public void shouldMaskMethodsNotInInterfaceWhenUsingField() throws Exception {
         InterfaceWithMethod myInterface = new ClassImplementingInterface("myPublic", "mySecret");
         ClassWithFieldInterface classWithFieldInterface = new ClassWithFieldInterface(myInterface);
-        JsonNode generated = JsonGenerator.generate(classWithFieldInterface, PojoMapOption.USE_INTERFACE_FIELDS);
+        JsonNode generated = JsonGenerator.generate(classWithFieldInterface);
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
         JsonObject childObj = jsonObject.requiredObject("myInterface");
@@ -237,7 +237,7 @@ public class JsonGeneratorTest {
         myList.add(new ClassImplementingInterface("mypub","myPriv"));
         classWithInterfaceListAndMapMethods.setMyList(myList);
 
-        JsonNode generated = JsonGenerator.generate(classWithInterfaceListAndMapMethods, PojoMapOption.USE_INTERFACE_FIELDS);
+        JsonNode generated = JsonGenerator.generate(classWithInterfaceListAndMapMethods);
 
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
@@ -256,7 +256,7 @@ public class JsonGeneratorTest {
         myMap.put("mykey",new ClassImplementingInterface("mypub","myPriv"));
         classWithInterfaceListAndMapMethods.setMyMap(myMap);
 
-        JsonNode generated = JsonGenerator.generate(classWithInterfaceListAndMapMethods, PojoMapOption.USE_INTERFACE_FIELDS);
+        JsonNode generated = JsonGenerator.generate(classWithInterfaceListAndMapMethods);
 
         assertThat(generated).isInstanceOf(JsonObject.class);
         JsonObject jsonObject = (JsonObject) generated;
