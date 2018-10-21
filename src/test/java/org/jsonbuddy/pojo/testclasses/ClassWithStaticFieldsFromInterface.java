392f142c99f2b64c3df5db6e6cfe372497a7f59e
Anders Karlsen <karlsenanders@gmail.com>
2018-10-21 12:17:15 +0200

    Added computed field to static field test as well to clarify further

--- src/test/java/org/jsonbuddy/pojo/testclasses/ClassWithStaticFieldsFromInterface.java
+++ src/test/java/org/jsonbuddy/pojo/testclasses/ClassWithStaticFieldsFromInterface.java
@@ -3,6 +3,11 @@ package org.jsonbuddy.pojo.testclasses;
 public class ClassWithStaticFieldsFromInterface implements InterfaceWithStaticFields {
 
     public static final String CONSTANT = "some constant";
+    public static final String COMPUTED_FIELD = computeField();
+
+    private static String computeField() {
+        return "computed";
+    }
 
     private String name;
 
