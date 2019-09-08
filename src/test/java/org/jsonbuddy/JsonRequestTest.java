937ccafe047d0fdf4caee45645c1abba81520ac1
Johannes Brodwall <johannes@brodwall.com>
2019-09-08 22:00:28 +0200

    Add tests for HTTP methods

--- src/test/java/org/jsonbuddy/JsonRequestTest.java
+++ src/test/java/org/jsonbuddy/JsonRequestTest.java
@@ -4,51 +4,90 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.URL;
-import java.net.URLConnection;
 
+import com.sun.net.httpserver.HttpServer;
 import org.assertj.core.api.AbstractThrowableAssert;
 import org.jsonbuddy.parse.JsonHttpException;
 import org.jsonbuddy.parse.JsonParser;
-import org.junit.Ignore;
 import org.junit.Test;
-@Ignore("Using external https. NONONO!!!")
+
 public class JsonRequestTest {
 
     @Test
     public void shouldGetJson() throws IOException {
-        JsonObject o = JsonParser.parseToObject(new URL("https://httpbin.org/json"));
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 0), 0);
+        httpServer.start();
+        httpServer.createContext("/", exchange -> {
+            exchange.getResponseHeaders().add("Content-type", "application/json; charset=utf-8");
+            exchange.sendResponseHeaders(200, 0);
+            try (PrintWriter printWriter = new PrintWriter(exchange.getResponseBody())) {
+                new JsonObject().put("slideshow", true).toJson(printWriter);
+            }
+            exchange.close();
+        });
+        JsonObject o = JsonParser.parseToObject(new URL("http://localhost:" + httpServer.getAddress().getPort()));
         assertThat(o.keys()).contains("slideshow");
     }
 
     @Test
-    public void shouldHandleJsonInErrors() throws IOException {
-        // This URL used by Google Oauth2 is used for POST and returns 405 when used with GET
-        URL url = new URL("https://accounts.google.com/o/oauth2/token");
-        URLConnection connection = url.openConnection();
+    public void shouldGetJsonArray() throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 0), 0);
+        httpServer.start();
+        httpServer.createContext("/", exchange -> {
+            exchange.getResponseHeaders().add("Content-type", "application/json; charset=utf-8");
+            exchange.sendResponseHeaders(200, 0);
+            try (PrintWriter printWriter = new PrintWriter(exchange.getResponseBody())) {
+                new JsonArray().add("slideshow").add("corn").toJson(printWriter);
+            }
+            exchange.close();
+        });
+        JsonArray a = JsonParser.parseToArray(new URL("http://localhost:" + httpServer.getAddress().getPort()));
+        assertThat(a.strings()).contains("slideshow", "corn");
+    }
 
-        AbstractThrowableAssert<?,?> exception = assertThatThrownBy(() -> JsonParser.parseToObject(connection))
-            .isInstanceOf(JsonHttpException.class)
-            .hasMessageContaining("404 Not Found")
-            .hasMessageContaining(url.toString());
-//        exception
-//            .extracting("jsonError")
-//            .containsOnly(new JsonObject().put("error", "invalid_request"));
+
+    @Test
+    public void shouldHandleJsonInErrors() throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 0), 0);
+        httpServer.start();
+        int serverPort = httpServer.getAddress().getPort();
+
+        httpServer.createContext("/", exchange -> {
+            exchange.getResponseHeaders().add("Content-type", "application/json; charset=utf-8");
+            exchange.sendResponseHeaders(400, 0);
+            try (PrintWriter printWriter = new PrintWriter(exchange.getResponseBody())) {
+                new JsonObject().put("error", "invalid_request").toJson(printWriter);
+            }
+            exchange.close();
+        });
+
+        URL url = new URL("http://localhost:" + serverPort);
+
+        AbstractThrowableAssert<?,?> exception = assertThatThrownBy(() -> JsonParser.parseToObject(url))
+            .isInstanceOf(JsonHttpException.class);
+        exception
+            .extracting("jsonError")
+            .containsOnly(new JsonObject().put("error", "invalid_request"));
     }
 
     @Test
     public void shouldHandleTextInErrors() throws IOException {
-        // This URL used by Google Oauth2 is used for browser redirects and returns an error page on missing parameters
-        URL url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
-        URLConnection connection = url.openConnection();
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 0), 0);
+        httpServer.start();
+        int serverPort = httpServer.getAddress().getPort();
 
-        AbstractThrowableAssert<?,?> exception = assertThatThrownBy(() -> JsonParser.parseToObject(connection))
+        URL url = new URL("http://localhost:" + serverPort);
+
+        AbstractThrowableAssert<?,?> exception = assertThatThrownBy(() -> JsonParser.parseToArray(url))
             .isInstanceOf(JsonHttpException.class)
-            .hasMessageContaining("400 Bad Request")
+            .hasMessageContaining("404 Not Found")
             .hasMessageContaining(url.toString());
         exception
             .extracting("errorContent")
-            .anyMatch(s -> s.toString().contains("<p id=\"errorDescription\">Required parameter is missing: response_type</p>"));
+            .anyMatch(s -> s.toString().contains("<h1>404 Not Found</h1>No context found for request"));
     }
 
 }
